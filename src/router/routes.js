9714aa97c889797112638468c46df6672317e0e7
Fabiola Castaneda <fabiscg79@gmail.com>
2025-02-16 14:30:59 -0500

    nothing to commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,22 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    name: "home",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+  },
+  {
+    path: "/recipes",
+    component: () => import("pages/RecipePage.vue"),
+    name: "recipes",
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
